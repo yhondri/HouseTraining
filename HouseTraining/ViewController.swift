9b86c8f1dc27ba7a52110217570cdc69bcb0d34d
Yhondri <yhondri@gmail.com>
2020-07-16 16:55:48 +0200

    Initial Commit

---
+++ HouseTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HouseTraining
+//
+//  Created by Yhondri Acosta Novas on 16/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
