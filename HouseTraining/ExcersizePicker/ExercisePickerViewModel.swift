600e1fce1eabb9a0fae313f2929b36e3974c7dba
Yhondri <yhondri@gmail.com>
2020-08-11 14:28:21 +0200

    Refactor ExercisePickerTableViewController.swift. Create ExercisePickerViewModel.

---
+++ HouseTraining/ExcersizePicker/ExercisePickerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExercisePickerViewModel.swift
+//  HouseTraining
+//
+//  Created by Yhondri Acosta Novas on 11/8/20.
+//
+
+
+public class ExercisePickerViewModel {
+    let reuseIdentifier = "CellReuseIdentifier"
+    let exercises = ["Jumping Jacks", "Abdominal Crunches"]
+}
